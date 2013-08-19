70e231f35272abfb579eab1fd00d1c41b0e4def8
Walter Lee Davis <waltd@wdstudio.com>
2013-08-19 18:18:20 -0400

    refactored to use SimpleXML

--- extract.php
+++ extract.php
@@ -1,16 +1,36 @@
 <?php
-ini_set('display_errors', true);
-error_reporting(E_ALL);
+require('config.inc.php');
 $tpl = file_get_contents('page.html');
-$pat = '/<([^>]+)>([^<]*?)\{\{(.+?)\}\}/';
-$matches = $output = array();
-preg_match_all($pat, $tpl, &$matches);
+$title = 'Title';
+$headline = 'Headline';
+$date = 'Date';
+$body = '##This is the body.
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+Duis aute irure dolor in "reprehenderit" in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+
+- One
+- Two
+- Three
+
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 header('Content-type: text/plain; charset=utf-8');
-foreach($matches[3] as $k => $v){
-  $out[$k] = $v;
-  if(preg_match('/data\-format/', $matches[1][$k])){
-    $out[$k] .= ' (' . preg_replace('/^.+?data\-format="(.+?)".*$/', '$1', $matches[1][$k]) . ')';
+$xml = simplexml_load_string($tpl);
+$variables = $xml->xpath('//title|//*[@data-format]');
+foreach($variables as $variable){
+  $key = trim((string) $variable->{0}, " \t\n\r\0\x0B{}");
+  $format = 'string';
+  if($variable->attributes() && $variable->attributes()->{'data-format'}){
+    $format = (string) $variable->attributes()->{'data-format'}->{0};
+    unset($variable->attributes()->{'data-format'});
   }
+  $part = call_user_func($format,$GLOBALS[$key]);
+  if(preg_match('/</', $part)){
+    $part = simplexml_load_string("<root>{$part}</root>");
+  }
+  $variable->{0} = $part;
+  print "{$key} ({$format})\n";
 }
-print_r($out);
+print tidy($xml->asXML());
 ?>
