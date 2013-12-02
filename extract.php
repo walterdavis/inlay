686912a0d5aab126306145bd09b204fb359af2d3
Walter Lee Davis <waltd@wdstudio.com>
2013-12-02 13:09:34 -0500

    support ajax editor, refactor data source

--- extract.php
+++ extract.php
@@ -1,23 +1,18 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
 require('config.inc.php');
 $tpl = file_get_contents('page.html');
-$title = 'Title';
-$headline = 'Headline';
-$date = 'Date';
-$body = '##This is the body.
-
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-
-Duis aute irure dolor in "reprehenderit" in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-
-- One
-- Two
-- Three
-
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
-header('Content-type: text/plain; charset=utf-8');
+//header('Content-type: text/plain; charset=utf-8');
 $xml = simplexml_load_string($tpl);
 $variables = $xml->xpath('//title|//*[@data-format]');
+$head = $xml->head[0];
+$script = $head->addChild('script');
+$script->addAttribute('type', 'text/javascript');
+$script->addAttribute('src', 'http://ajax.googleapis.com/ajax/libs/prototype/1.7/prototype.js');
+$script = $head->addChild('script');
+$script->addAttribute('type', 'text/javascript');
+$script->addAttribute('src', 'javascripts/editor.js');
 $substitutes = array();
 foreach($variables as $k => $variable){
   $key = trim((string) $variable->{0}, " \t\n\r\0\x0B{}");
@@ -25,8 +20,11 @@ foreach($variables as $k => $variable){
   if($variable->attributes() && $variable->attributes()->{'data-format'}){
     $format = (string) $variable->attributes()->{'data-format'}->{0};
     unset($variable->attributes()->{'data-format'});
+    $variable->addAttribute('data-source',$key);
+    add_class_name($variable, 'editable');
+    add_class_name($variable, $format);
   }
-  $substitutes[$k] = call_user_func($format,$GLOBALS[$key]);
+  $substitutes[$k] = call_user_func($format,$params[$key]);
   $variable[0][0] = '%s';
 }
 print tidy(vsprintf($xml->asXML(), $substitutes));
