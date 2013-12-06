b82d96a911b7e25d7b4a0cd0d149a92bec6d56b1
Walter Lee Davis <waltd@wdstudio.com>
2013-12-06 08:46:11 -0500

    experimenting with dynamic routing

---
+++ routing.php
@@ -0,0 +1,8 @@
+<?php
+$tpl = (string) $_GET['page'] . '.html';
+if(file_exists('./' + $tpl)){
+  $tpl = file_get_contents('./' + $tpl);
+}
+header('Content-type: text/html; charset=utf-8');
+print $tpl;
+?>
