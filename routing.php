773234562f317163c6a6597f06c716d2b4c6f121
Walter Lee Davis <waltd@wdstudio.com>
2013-12-06 08:51:50 -0500

    fixed rewrites and concatenation operators

--- routing.php
+++ routing.php
@@ -1,7 +1,7 @@
 <?php
 $tpl = (string) $_GET['page'] . '.html';
-if(file_exists('./' + $tpl)){
-  $tpl = file_get_contents('./' + $tpl);
+if(file_exists('./' . $tpl)){
+  $tpl = file_get_contents('./' . $tpl);
 }
 header('Content-type: text/html; charset=utf-8');
 print $tpl;
