113fd2e7c2edfb9d54ae2af8995fbaf4dcd2d2b8
Walter Lee Davis <waltd@wdstudio.com>
2013-12-10 21:19:31 -0500

    crypto salt

--- config.inc.php
+++ config.inc.php
@@ -1,9 +1,10 @@
 <?php
 ini_set('display_errors', true);
 error_reporting(E_ALL);
+$salt = 'WWJlcnogdmNmaHogcWJ5YmUgZnZnIG56cmcsIHBiYWZycGdyZ2hlIG5xdmN2ZnZwdmF0IHJ5dmc=';
 require('lib/markdown.php');
 require('lib/smartypants.php');
 require('lib/tidy.php');
 require('lib/functions.php');
-require('test_data.php');
+
 ?>
