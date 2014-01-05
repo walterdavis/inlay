c2af9a32fa438bd95c017d4cbeeb7dd93cc93559
Walter Lee Davis <waltd@wdstudio.com>
2014-01-05 10:20:28 -0500

    stubbed out virtual page support in routes

--- routing.php
+++ routing.php
@@ -4,6 +4,8 @@ $template = '';
 $template_path = (string) $_GET['page'] . '.html';
 if(file_exists('./' . $template_path)){
   $template = file_get_contents('./' . $template_path);
+}elseif(true == false){
+  // put database lookup here to find virtual page
 }else{
   header("HTTP/1.0 404 Not Found", true, 404);
   print '<h1>Missing</h1><p>Sorry, that file could not be found.</p>';
