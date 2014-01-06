3693dd227c49d60ed4f95b3183361f268b665df9
Walter Lee Davis <waltd@wdstudio.com>
2014-01-06 09:13:38 -0500

    renamed value

---
+++ models/element.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  CREATE TABLE `elements` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `signature` char(32) NOT NULL DEFAULT '',
+    `source` varchar(255) NOT NULL DEFAULT '',
+    `format` varchar(255) DEFAULT 'string',
+    `content` text,
+    `created_at` datetime NOT NULL,
+    `updated_at` datetime NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+*/
+class Element extends MiniActiveRecord{
+  public $validations = 'presence:key; presence:source; presence:format';
+}
+?>
