3bade9158cc2e320d31f2f1b2cfb523d05db406d
Walter Lee Davis <waltd@wdstudio.com>
2014-01-10 12:32:07 -0500

    refactored data sources

--- javascripts/editor.js
+++ javascripts/editor.js
@@ -2,20 +2,20 @@ document.observe('dom:loaded', function(){
   $('PageDiv').setStyle('padding-top: 40px;');
   $(document.body).insert('<div class="title-eyebrow"><a id="back-to-show" href="' + window.location.href.split('?').first() + '">⇦</a><div class="editable" id="page-title" data-source="title" data-format="string">' + document.title + '</div></div>');
   $('page-title').setStyle('width:' + $('PageDiv').getStyle('width') + '; margin: auto; font:' + $('PageDiv').getStyle('font'))
-  $$('.editable').invoke('observe', 'dblclick', function(evt){
+  $$('.editable').invoke('observe', 'click', function(evt){
     var elm = this, editor, txt;
     if(elm.down('.editor')) return;
     txt = elm.innerHTML.toString().trim();
-    if(elm.hasClassName('markdown')){
+    if(elm.readAttribute('data-format') == 'markdown'){
       editor = new Element('textarea', {'class':'editor'});
     }else{
       editor = new Element('input', {type: 'text', 'class':'editor'});
     }
     editor.writeAttribute('name', elm.readAttribute('data-source'));
+    var data_key = (editor.name == 'title') ? $$('head title').first().readAttribute('data-key') : elm.readAttribute('data-key');
     new Ajax.Request('get_raw.php', {
       parameters: {
-        source: window.location.pathname.sub('/fwCMS/', ''),
-        key: elm.readAttribute('data-source')
+        key: data_key
       },
       onSuccess: function(xhr){
         editor.setValue(xhr.responseText);
@@ -23,12 +23,13 @@ document.observe('dom:loaded', function(){
       }
     });
     editor.observe('blur', function(){
+      var data_key = (editor.name == 'title') ? $$('head title').first().readAttribute('data-key') : elm.readAttribute('data-key');
       new Ajax.Updater(elm, 'set_raw.php', {
         parameters: {
-          key: editor.name,
+          key: data_key,
+          source: elm.readAttribute('data-source'),
           val: editor.getValue(),
-          source: window.location.pathname.sub('/fwCMS/', ''),
-          markdown: (!!elm.hasClassName('markdown'))
+          format: elm.readAttribute('data-format')
         },
         onSuccess: function(xhr){
           if(editor.name == 'title') document.title = xhr.responseText;
