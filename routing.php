3be5546e26b790919ef2ce1820d877697f57b7ea
Walter Lee Davis <waltd@wdstudio.com>
2014-01-10 12:26:00 -0500

    detect virtual pages, refactor to use Template class

--- routing.php
+++ routing.php
@@ -1,19 +1,22 @@
 <?php
 require('config.inc.php');
-$template = '';
-$template_path = (string) $_GET['page'] . '.html';
+$template_path = (string) $_GET['path'];
+if(substr($template_path, -5) != '.html'){
+  $template_path = (substr($template_path, -1) == '/') ? $template_path . 'index.html' : $template_path . '/index.html';
+}
 if(file_exists('./' . $template_path)){
-  $template = file_get_contents('./' . $template_path);
-}elseif(true == false){
+  $template = new Template($template_path);
+}elseif(present('virtual', $_GET)){
   // put database lookup here to find virtual page
+  print 'virtual';
+  exit;
 }else{
   header("HTTP/1.0 404 Not Found", true, 404);
-  print '<h1>Missing</h1><p>Sorry, that file could not be found.</p>';
+  print '<html><title>404 Not Found</title><body><h1>Not Found</h1><p>The requested URL ' . $_SERVER['REQUEST_URI'] . ' was not found on this server.</p><hr />' . $_SERVER['SERVER_SIGNATURE'] . '</body></html>';
   exit;
 }
 if(present('raw', $_GET)){
   header('Content-type: text/plain; charset=utf-8');
-  print $template;
   exit;
 }elseif(present('edit',$_GET)){
   require('edit.php');
