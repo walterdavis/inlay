76c48ffdaf10d51ab222a9a1fe61bb411b51a1d1
Walter Lee Davis <waltd@wdstudio.com>
2014-01-10 12:29:31 -0500

    refactored to use database

--- get_raw.php
+++ get_raw.php
@@ -1,9 +1,10 @@
 <?php
 require( 'config.inc.php' );
-$template_path = (string)$_POST['source'];
-$template = file_get_contents('./' . $template_path);
-$xml = simplexml_load_string($template);
-require('test_data.php');
+require('models/element.php');
+$element = new Element();
 header('Content-type: text/plain; charset=utf-8');
-print $params[$_POST['key']];
+$e = $element->find_by_signature($_POST['key']);
+if($e){
+  print $e->content;
+}
 ?>
--- set_raw.php
+++ set_raw.php
@@ -1,12 +1,21 @@
 <?php
 require( 'config.inc.php' );
-$template_path = (string)$_POST['source'];
-$crypt = md5($_SERVER['HTTP_HOST'] . '/fwCMS/' . $template_path . $_POST['key'] . $salt);
-file_put_contents('./data/' . $crypt . '.txt', $_POST['val']);
+require('models/element.php');
+$element = new Element();
+$e = $element->find_by_signature($_POST['key']);
+if(!$e){
+  $e = $element->build(array('signature' => $_POST['key']));
+}
+if($e){
+  $e->content = $_POST['val'];
+  $e->source = $_POST['source'];
+  $e->format = $_POST['format'];
+  $e->save();
+}
 header('Content-type: text/html; charset=utf-8');
-if(isset($_POST['markdown']) && $_POST['markdown'] == 'true'){
-  print markdown($_POST['val']);
+if($_POST['format'] == 'markdown'){
+  print markdown($e->content);
   exit;
 }
-print $_POST['val'];
+print $e->content;
 ?>
