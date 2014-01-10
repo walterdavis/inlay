4a88a08a70841216e8add15b4f96125d99c19a90
Walter Lee Davis <waltd@wdstudio.com>
2014-01-10 12:28:10 -0500

    refactor to use Template class, add timing

--- edit.php
+++ edit.php
@@ -1,22 +1,38 @@
 <?php
-$xml = simplexml_load_string($template);
-require('test_data.php');
-$head = $xml->head[0];
+require('models/element.php');
+header('Content-type: text/html; charset=utf-8');
+$substitutes = array();
+$element = new Element();
+foreach($template->variables as $key => $val){
+  $e = $element->find_by_signature($key);
+  if($e){
+    $substitutes[$e->source] = call_user_func($e->format, $e->content);
+  }else{
+    $substitutes[$val['source']] = Inflector::humanize($val['source']) . ' is not defined.';
+  }
+}
+$head = $template->xml->head[0];
 $script = $head->addChild('script');
 $script->addAttribute('type', 'text/javascript');
 $script->addAttribute('src', 'http://ajax.googleapis.com/ajax/libs/prototype/1.7/prototype.js');
 $script = $head->addChild('script');
 $script->addAttribute('type', 'text/javascript');
 $script->addAttribute('src', 'javascripts/editor.js');
-foreach($variables as $k => $variable){
-  $key = (string) $variable->attributes()->{'data-source'}->{0};
-  $format = 'string';
-  if($variable->attributes() && $variable->attributes()->{'data-format'} && $variable->attributes()->{'data-format'}->{0}){
-    $format = (string) $variable->attributes()->{'data-format'}->{0};
-    unset($variable->attributes()->{'data-format'});
-    add_class_name($variable, 'editable');
-    add_class_name($variable, $format);
+$template->xml->body[0]->addAttribute('data-key', $template->template_key);
+foreach($template->fields as $k => $field){
+  if($field->attributes() && $field->attributes()->{'data-format'} && $field->attributes()->{'data-format'}->{0}){
+    $format = (string) $field->attributes()->{'data-format'}->{0};
+    add_class_name($field, 'editable');
+    add_class_name($field, $format);
   }
 }
-print tidy(vsprintf($xml->asXML(), $substitutes));
+print clean_output($template->populate($substitutes, false));
+$end=microtime(); 
+$end=explode(" ",$end); 
+$end=$end[1]+$end[0]; 
+if(MAR_DEVELOPER_MODE) printf("<!-- Page generated in %f seconds. -->",$end-$start); 
+
+
+
+
 ?>
--- show.php
+++ show.php
@@ -1,16 +1,19 @@
 <?php
+require('models/element.php');
 header('Content-type: text/html; charset=utf-8');
-$xml = simplexml_load_string($template);
-require('test_data.php');
-foreach($variables as $k => $variable){
-  if($variable->attributes()){
-    unset($variable->attributes()->{'data-format'});
-    unset($variable->attributes()->{'data-source'});
+$substitutes = array();
+$element = new Element();
+foreach($template->variables as $key => $val){
+  $e = $element->find_by_signature($key);
+  if($e){
+    $substitutes[$e->source] = call_user_func($e->format, $e->content);
+  }else{
+    $substitutes[$val['source']] = Inflector::humanize($val['source']) . ' is not defined.';
   }
 }
-$body = $xml->body[0];
-$link = $body->addChild('a');
-$link->addAttribute('class', 'edit-badge');
-$link->addAttribute('href', '?edit=' . time());
-print tidy(vsprintf($xml->asXML(), $substitutes));
+print clean_output($template->populate($substitutes));
+$end=microtime(); 
+$end=explode(" ",$end); 
+$end=$end[1]+$end[0]; 
+if(MAR_DEVELOPER_MODE) printf("<!-- Page generated in %f seconds. -->",$end-$start); 
 ?>
