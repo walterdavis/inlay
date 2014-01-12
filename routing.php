b2183af243b46c750eaaf6ca98c4ff4b197f3a52
Walter Lee Davis <waltd@wdstudio.com>
2014-01-12 12:59:20 -0500

    refactor to use pages lookup for virtual pages

--- routing.php
+++ routing.php
@@ -1,19 +1,23 @@
 <?php
 require('config.inc.php');
 $template_path = (string) $_GET['path'];
+
 if(substr($template_path, -5) != '.html'){
   $template_path = (substr($template_path, -1) == '/') ? $template_path . 'index.html' : $template_path . '/index.html';
 }
 if(file_exists('./' . $template_path)){
   $template = new Template($template_path);
 }elseif(present('virtual', $_GET)){
-  // put database lookup here to find virtual page
-  print 'virtual';
-  exit;
+  require('models/page.php');
+  $page = new Page();
+  $signature = md5($_SERVER['SERVER_NAME'] . ROOT_FOLDER . $template_path . SALT);
+  if($p = $page->find_by_signature($signature)){
+    $template = new Template($p->template);
+  }else{
+    missing();
+  }
 }else{
-  header("HTTP/1.0 404 Not Found", true, 404);
-  print '<html><title>404 Not Found</title><body><h1>Not Found</h1><p>The requested URL ' . $_SERVER['REQUEST_URI'] . ' was not found on this server.</p><hr />' . $_SERVER['SERVER_SIGNATURE'] . '</body></html>';
-  exit;
+  missing();
 }
 if(present('raw', $_GET) && MAR_DEVELOPER_MODE){
   header('Content-type: text/plain; charset=utf-8');
