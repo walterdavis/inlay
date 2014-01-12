326902038d24b6fb4bcbacf7f716c3f7b5e363b7
Walter Lee Davis <waltd@wdstudio.com>
2014-01-12 12:59:48 -0500

    update sql example

--- models/element.php
+++ models/element.php
@@ -3,13 +3,14 @@
   CREATE TABLE `elements` (
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `signature` char(32) NOT NULL DEFAULT '',
-    `source` varchar(255) NOT NULL DEFAULT '',
+    `source` varchar(255) DEFAULT '',
     `format` varchar(255) DEFAULT 'string',
     `content` text,
     `created_at` datetime NOT NULL,
     `updated_at` datetime NOT NULL,
-    PRIMARY KEY (`id`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+    PRIMARY KEY (`id`),
+    KEY `signature` (`signature`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 */
 class Element extends MiniActiveRecord{
   public $validations = 'presence:signature';
