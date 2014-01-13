6bd957d0dd6d4265548de4f4d8f46c1b3019dba0
Walter Lee Davis <waltd@wdstudio.com>
2014-01-12 21:55:11 -0500

    put correct SQL in the comment

--- models/page.php
+++ models/page.php
@@ -1,16 +1,16 @@
 <?php
 /*
-  CREATE TABLE `elements` (
+  CREATE TABLE `pages` (
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `signature` char(32) NOT NULL DEFAULT '',
-    `source` varchar(255) DEFAULT '',
-    `format` varchar(255) DEFAULT 'string',
-    `content` text,
+    `server` varchar(255) DEFAULT '',
+    `template` varchar(255) DEFAULT '',
+    `path` varchar(1024) DEFAULT NULL,
     `created_at` datetime NOT NULL,
     `updated_at` datetime NOT NULL,
     PRIMARY KEY (`id`),
     KEY `signature` (`signature`)
-  ) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 */
 
 class Page extends MiniActiveRecord{
