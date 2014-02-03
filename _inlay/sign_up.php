aa9027d04f8713ab73e03b9a004ff41ab302fe9b
Walter Lee Davis <waltd@wdstudio.com>
2014-02-03 15:21:25 -0500

    new user stylesheet, removed short-open tags

--- _inlay/sign_up.php
+++ _inlay/sign_up.php
@@ -23,7 +23,7 @@ if(present('commit', $_POST)){
 <head>
   <meta charset="utf-8"/>
   <title>Sign Up</title>
-  <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" charset="utf-8"/>
+  <link rel="stylesheet" href="css/user.css" type="text/css" media="screen" charset="utf-8"/>
 </head>
 <body>
   <div id="PageDiv">
@@ -35,9 +35,9 @@ if(present('commit', $_POST)){
           printf($flash, 'error', $error);
         }
         ?>
-        <p><label for="first_name">First name</label><input name="first_name" id="first_name" autofocus="autofocus" value="<?= $user->first_name ?>"/></p>
-        <p><label for="last_name">Last name</label><input name="last_name" id="last_name" value="<?= $user->last_name ?>"/></p>
-        <p><label for="email">E-mail</label><input type="email" name="email" id="email" value="<?= $user->email ?>"/></p>
+        <p><label for="first_name">First name</label><input name="first_name" id="first_name" autofocus="autofocus" value="<?php echo $user->first_name; ?>"/></p>
+        <p><label for="last_name">Last name</label><input name="last_name" id="last_name" value="<?php echo $user->last_name; ?>"/></p>
+        <p><label for="email">E-mail</label><input type="email" name="email" id="email" value="<?php echo $user->email; ?>"/></p>
         <p><label for="password">Password</label><input type="password" name="password" id="password" value=""/></p>
         <p><label for="password_confirmation">Confirm password</label><input type="password" name="password_confirmation" id="password_confirmation" value=""/></p>
         <p><input type="submit" name="commit" value="Enter"/></p>
