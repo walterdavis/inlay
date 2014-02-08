8dcbe8bdd26a3c672cc600ebc1fdf22dfae10eb5
Walter Lee Davis <waltd@wdstudio.com>
2014-02-08 17:46:25 -0500

    ROOT_PATH now works in both subfolder and site root locations

--- _inlay/config.inc.php
+++ _inlay/config.inc.php
@@ -27,10 +27,7 @@ require('lib/HTML5/Parser.php');
 require 'lib/Template.php';
 // root all paths to the folder containing the _inlay.php file
 define('ROOT', dirname(dirname(__FILE__)));
-$root_folder = str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT) . '/';
-if($root_folder == '/') $root_folder = '';
-define('ROOT_FOLDER', $root_folder);
-unset($root_folder);
+define('ROOT_FOLDER', str_replace($_SERVER['DOCUMENT_ROOT'], '', ROOT) . '/');
 session_start();
 $flash = '<div class="flash %s"><ul>%s</ul></div>';
 ?>
