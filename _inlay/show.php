3a8f85a77cb2d92a89e337ddef2ce3d9e9ba0964
Walter Lee Davis <waltd@wdstudio.com>
2014-02-15 16:56:12 -0500

    show the edit controls (by default at the moment)

--- _inlay/show.php
+++ _inlay/show.php
@@ -18,6 +18,22 @@ $top = $xpath->query('//head/*[1]');
 $base = $template->doc->createElement('base');
 $base->setAttribute('href', $template->base);
 $head->item(0)->insertBefore($base, $top->item(0));
+if(1 == 1 && count($template->fields) > 0){
+  $style = $head->item(0)->appendChild($template->doc->createElement('link'));
+  $style->setAttribute('type', 'text/css');
+  $style->setAttribute('rel', 'stylesheet');
+  $style->setAttribute('href', '/' . join_path(array(ROOT_FOLDER , '/_inlay/css/show.css')));
+  $body = $xpath->query('//body');
+  $bar = $template->doc->createElement('div');
+  $bar->setAttribute('class', 'title-show-bar');
+  $body->item(0)->appendChild($bar);
+  $button = $template->doc->createElement('a');
+  $button->setAttribute('id', 'inlay-icon');
+  $button->setAttribute('href', $_SERVER['REQUEST_URI'] . '?edit=true');
+  $button->setAttribute('title', 'Edit this page');
+  $button->nodeValue = 'i';
+  $bar->appendChild($button);  
+}
 print clean_output($template->populate($substitutes));
 if(MAR_DEVELOPER_MODE) printf("<!-- Generated with love, by Inlay, in %f seconds. -->", timing()); 
 ?>
