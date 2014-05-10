d29f8f9c5c61b142d3a76581150f38320f38cff7
Walter Lee Davis <waltd@wdstudio.com>
2014-05-10 14:06:33 -0400

    new structure

--- _inlay/bootstrap.sql
+++ _inlay/bootstrap.sql
@@ -3,7 +3,7 @@
 CREATE TABLE `collections` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
-  `server` varchar(255) NOT NULL DEFAULT '',
+  `site_id` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
@@ -11,6 +11,7 @@ CREATE TABLE `collections` (
 
 CREATE TABLE `elements` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `site_id` int(11) NOT NULL,
   `signature` char(32) NOT NULL DEFAULT '',
   `source` varchar(255) NOT NULL DEFAULT '',
   `format` varchar(255) NOT NULL DEFAULT 'string',
@@ -18,7 +19,8 @@ CREATE TABLE `elements` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `signature` (`signature`)
+  KEY `signature` (`signature`),
+  KEY `site_id` (`site_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `members` (
@@ -35,18 +37,31 @@ CREATE TABLE `members` (
 CREATE TABLE `pages` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `signature` char(32) NOT NULL DEFAULT '',
-  `server` varchar(255) NOT NULL DEFAULT '',
+  `site_id` int(11) NOT NULL,
   `template` varchar(255) NOT NULL DEFAULT '',
   `path` varchar(1024) NOT NULL DEFAULT '',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `signature` (`signature`)
+  KEY `signature` (`signature`),
+  KEY `site_id` (`site_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `sessions` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `sites` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `user_id` int(11) DEFAULT NULL,
+  `signature` varchar(255) NOT NULL DEFAULT '',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `signature` (`signature`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `users` (
